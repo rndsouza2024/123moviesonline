833ca81ab24d5943d901e983ecf3b225d114c62f
mxplayermovies <dsouzarnd@gmail.com>
2024-08-06 01:03:18 +0530

    update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://123moviesgo.vercel.app/',
+  siteUrl: 'https://123moviesonline.vercel.app/',
   changefreq: 'daily',
   priority: 0.9, // Set priority to just below the highest value
   // generateRobotsTxt: true,

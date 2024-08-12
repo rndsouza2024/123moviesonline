2661203a421547bead419663625d7a56c4517cf2
mxplayermovies <dsouzarnd@gmail.com>
2024-08-12 23:34:31 +0530

    UUP DOC

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,41 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-        
+        <link
+            rel='sitemap'
+            type='application/xml'
+            title='Sitemap'
+            href='https://123moviesonline.vercel.app/sitemap.xml'
+          />
+          <link
+            rel='icon'
+            type='image/x-icon'
+            href='/favicon.ico'
+          />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <meta name='googlebot' content='index,follow' />
+          <meta name='revisit-after' content='1 days' />
+          <meta
+            name='robots'
+            content='index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1'
+          />
           <link
             rel='stylesheet'
             href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css'

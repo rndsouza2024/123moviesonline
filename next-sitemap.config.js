be9bfa9d3a86af91f0e63e047b0fe6af31549b3a
mxplayermovies <dsouzarnd@gmail.com>
2024-08-20 16:43:49 +0530

    kick

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://123moviesonline.vercel.app/',
+    changefreq: 'daily',
+    priority: 0.90,
+    // generateRobotsTxt: true,
+    sitemapSize: 7000,
+  }
+
+

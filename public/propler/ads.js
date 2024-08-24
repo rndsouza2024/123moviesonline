65286cd2c0bf1a92571e0b2a6c04c59ed2c0500f
mxplayermovies <dsouzarnd@gmail.com>
2024-08-25 02:00:56 +0530

    ok get it

--- public/propler/ads.js
+++ public/propler/ads.js
@@ -1,33 +1,18 @@
-      // // Function to load the ad code
-      // function loadAdCode() {
-      //   var adScript = document.createElement('script');
-      //   adScript.async = true;
-      //   adScript.setAttribute('data-cfasync', 'false');
-      //   adScript.src = '//thubanoa.com/1?z=7762649';
-      //   document.body.appendChild(adScript);
-      // }
-
-      // // Set a timeout to load the ad code after 5 seconds
-      // setTimeout(loadAdCode, 8000); // 5000 milliseconds = 5 seconds
-
-
-      // // <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7762649"></script>
-
-      // Function to load the ad code
-function loadAdCode() {
-  var adScript = document.createElement('script');
-  adScript.src = 'https://alwingulla.com/88/tag.min.js';
-  adScript.setAttribute('data-zone', '92271');
-  adScript.async = true;
-  adScript.setAttribute('data-cfasync', 'false');
+// Function to load the ad code
+function loadAdCode () {
+  var adScript = document.createElement('script')
+  adScript.src = 'https://alwingulla.com/88/tag.min.js'
+  adScript.setAttribute('data-zone', '92271')
+  adScript.async = true
+  adScript.setAttribute('data-cfasync', 'false')
 
   // Handle script load errors
-  adScript.onerror = function() {
-    console.error('Failed to load ad script');
-  };
+  adScript.onerror = function () {
+    console.error('Failed to load ad script')
+  }
 
-  document.body.appendChild(adScript);
+  document.body.appendChild(adScript)
 }
 
 // Set a timeout to load the ad code after 15 seconds
-setTimeout(loadAdCode, 10000); // 15000 milliseconds = 15 seconds
+setTimeout(loadAdCode, 10000) // 15000 milliseconds = 15 seconds

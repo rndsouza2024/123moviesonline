f421c37a4932d0142e060546eda7e5dcb40d481e
mxplayermovies <dsouzarnd@gmail.com>
2024-08-28 23:16:34 +0530

    ok donation

--- pages/_app.js
+++ pages/_app.js
@@ -8,19 +8,28 @@ import { useEffect } from 'react';
 
 function Application({ Component, pageProps }) {
   useEffect(() => {
-    const setupWebpushr = () => {
-      if (typeof window.webpushr !== 'undefined') {
-        window.webpushr('setup', {
-          key: 'BIHpgrvLvdxGSRA7cHudMTBdr7EWGon3q4reCUGbDcm5uiM2CkypC83diBbYhTMaD8pY_5G0L817DCPB3UqY2CI',
+    // Add the Ko-fi widget script to the page
+    const script = document.createElement('script');
+    script.src = 'https://storage.ko-fi.com/cdn/scripts/overlay-widget.js';
+    script.async = true;
+
+    script.onload = () => {
+      if (typeof kofiWidgetOverlay !== 'undefined') {
+        kofiWidgetOverlay.draw('payat', {
+          'type': 'floating-chat',
+          'floating-chat.donateButton.text': 'Support me',
+          'floating-chat.donateButton.background-color': '#00b9fe',
+          'floating-chat.donateButton.text-color': '#fff'
         });
       }
     };
 
-    const script = document.createElement('script');
-    script.src = 'https://cdn.webpushr.com/app.min.js';
-    script.async = true;
-    script.onload = setupWebpushr;
     document.body.appendChild(script);
+
+    return () => {
+      // Cleanup if the component is unmounted
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
